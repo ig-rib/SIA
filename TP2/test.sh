a172205ebb1244a8a1845751fcf6d5d8feb234c8
gmarchetti96 <gmarchetti@itba.edu.ar>
2020-04-24 00:29:24 -0300

    another plot fix

--- TP2/test.sh
+++ TP2/test.sh
@@ -6,9 +6,8 @@ do
 printf "Start Test ${i}\n"
 cat solver.config.${i} > solver.config
 python3 main.py > test${i}.txt
-./plot.sh ${i}
 printf "Test ${i} Finished\n"
 
 #Plot Generator
-print "set title \'Test ${i}\'\nset grid\nset ylabel \'Fitness\'\nset xlabel \'Generation\'\nset term png size 1024,768\nset output \'plot-test${i}.png\'\nset style fill transparent solid .2 noborder\nplot \"test${i}.txt\" with points" | gnuplot
+printf "set title \'Test ${i}\'\nset grid\nset ylabel \'Fitness\'\nset xlabel \'Generation\'\nset term png size 1024,768\nset output \'plot-test${i}.png\'\nset style fill transparent solid .2 noborder\nplot \"test${i}.txt\" with points" | gnuplot
 done
