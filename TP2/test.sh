d77b1386aee1d8deb579caed126d5661d5251482
gmarchetti96 <gmarchetti@itba.edu.ar>
2020-04-24 00:24:29 -0300

    plot script fix

--- TP2/test.sh
+++ TP2/test.sh
@@ -2,8 +2,13 @@
 echo "Start Test"
 for i in 1 2 3 4
 do
+#Runtime
 printf "Start Test ${i}\n"
 cat solver.config.${i} > solver.config
 python3 main.py > test${i}.txt
+./plot.sh ${i}
 printf "Test ${i} Finished\n"
+
+#Plot Generator
+print "set title \'Test ${i}\'\nset grid\nset ylabel \'Fitness\'\nset xlabel \'Generation\'\nset term png size 1024,768\nset output \'plot-test${i}.png\'\nset style fill transparent solid .2 noborder\nplot \"test${i}.txt\" with points" | gnuplot
 done
