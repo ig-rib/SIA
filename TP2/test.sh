a955beaa00a02765c63face22ffd650de55c93c9
Gianfranco Marchetti <gmarchetti@pampero.it.itba.edu.ar>
2020-04-24 01:23:01 -0300

    ready to test

--- TP2/test.sh
+++ TP2/test.sh
@@ -10,5 +10,5 @@ printf "Test ${i} Finished\n"
 
 #Plot Generator
 sub=`cat solver.config | sed ':a;N;$!ba;s/\n/ - /g'`
-printf "set title \'${sub}\'\nset grid\nset ylabel \'Fitness\'\nset xlabel \'Generation\'\nset term png size 1024,768\nset output \'plot-test${i}.png\'\nset style fill transparent solid .2 noborder\nplot \"test${i}.txt\" with points" | gnuplot
+printf "set title \'${sub}\'\nset grid\nset ylabel \'Fitness\'\nset xlabel \'Generation\'\nset term png size 1600,900\nset output \'plot-test${i}.png\'\nset style fill transparent solid .2 noborder\nplot \"test${i}.txt\" with points" | gnuplot
 done
