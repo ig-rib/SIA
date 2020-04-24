ef9b1a467d4eed2e53af05b36729cf07fc17a438
gmarchetti96 <gmarchetti@itba.edu.ar>
2020-04-24 17:07:02 -0300

    test reduce options

--- TP2/test.sh
+++ TP2/test.sh
@@ -9,7 +9,8 @@ python3 main.py > test${i}.txt
 printf "Test ${i} Finished\n"
 
 #Plot Generator
-sub=`cat solver.config | sed ':a;N;$!ba;s/\n/ - /g'`
+sub=`cat config/solver.config.0 | sed ':a;N;$!ba;s/\n/ - /g' | sed ':s;s/\(\<\S*\>\)\(.*\)\<\1\>/\1\2/g;ts'`
+
 printf "set title \'${sub}\'\nset grid\nset ylabel \'Fitness\'\nset xlabel \'Generation\'\nset term png size 1600,900\nset output \'plot-test${i}.png\'\nset style fill transparent solid .2 noborder\nplot \"test${i}.txt\" with points" | gnuplot
 
 i=$(($i + 1))
