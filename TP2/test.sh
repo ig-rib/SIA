faa0023995eec9fa6a9725c0f41d5b2340f86210
gmarchetti96 <gmarchetti@itba.edu.ar>
2020-04-24 13:51:02 -0300

    test fix index

--- TP2/test.sh
+++ TP2/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+i=$1
 echo "Start Test"
-for i in {$1..$2}
+while [ "$i" -le "$2" ]; do
 do
 #Runtime
 printf "Start Test ${i}\n"
@@ -11,4 +12,6 @@ printf "Test ${i} Finished\n"
 #Plot Generator
 sub=`cat solver.config | sed ':a;N;$!ba;s/\n/ - /g'`
 printf "set title \'${sub}\'\nset grid\nset ylabel \'Fitness\'\nset xlabel \'Generation\'\nset term png size 1600,900\nset output \'plot-test${i}.png\'\nset style fill transparent solid .2 noborder\nplot \"test${i}.txt\" with points" | gnuplot
+
+i=$(($i + 1))
 done
