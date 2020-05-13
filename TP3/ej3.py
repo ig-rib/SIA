5f5b0826f87d7c8ea899e90505f763df6eb7687b
igrib <igrib@example.com>
2020-05-13 14:27:35 -0300

    a working version for various problems in mlp

--- TP3/ej3.py
+++ TP3/ej3.py
@@ -6,6 +6,7 @@ import random
 import numpy as np
 import sys
 from multiLayerPerceptron import MultiLayerPerceptron, tanh, tanhPrime, logistic, logisticPrime
+import constants as ct
 
 setFileLines = open('ej3Conjunto.tsv').readlines()
 
@@ -20,7 +21,7 @@ setFileLines = open('ej3Conjunto.tsv').readlines()
 # X = list(numbers.values())
 # y = [ 1 if index == 2 or index == 3 or index == 5 or index == 7 else -1 for index, x in enumerate(X) ]
 
-# mlp = MultiLayerPerceptron(0.001, tanh, tanhPrime, 35, [30, 25, 20, 15, 10, 5, 2], 1,  a=1e-7, b=1e-4)
+# mlp = MultiLayerPerceptron(0.001, tanh, tanhPrime, 35, [4, 2], 1,  a=1e-7, b=1e-4)
 # mlp.train(X, y, adaptative=True)
 
 # for i in range(len(X)):
@@ -34,7 +35,7 @@ domain = [ [-1, 1], [1, -1], [-1, -1], [1, 1] ]
 XORys = [ -1 if x[0] == x[1] else 1 for x in domain ]
 domain[:] = [ np.asmatrix(x) for x in domain ]
 
-mlp = MultiLayerPerceptron(0.1, tanh, tanhPrime, 2, [4, 2], 1,  a=1e-7, b=1e-4)
+mlp = MultiLayerPerceptron(0.01, tanh, tanhPrime, 2, [4, 2], 1,  a=1e-7, b=1e-4, backProp=ct.BP_NO_PRIME, update=ct.UPDATE_MOMENTUM)
 mlp.train(domain, XORys, epochs=10000, adaptative=True)
 
 for i in range(len(domain)):
@@ -69,8 +70,51 @@ for i in range(len(domain)):
 X = [ np.asmatrix([ 1 if i == j else 0 for i in range(10) ]) for j in range(10) ]
 y = [ 1 if index == 2 or index == 3 or index == 5 or index == 7 else -1 for index, x in enumerate(X) ]
 
-mlp = MultiLayerPerceptron(0.01, tanh, tanhPrime, 10, [10, 4, 2], 1,  a=1e-7, b=1e-4)
+mlp = MultiLayerPerceptron(0.01, tanh, tanhPrime, 10, [10, 4, 2], 1,  a=1e-7, b=1e-4, backProp=ct.BP_NO_PRIME, update=ct.UPDATE_MOMENTUM)
 mlp.train(X, y, adaptative=True)
+
+for i in range(len(X)):
+    print(mlp.classify(X[i]), y[i])
+
+#-----------------------------------------------------#
+# RECONOCE NUMEROS EXPRESADOS EN VECTORES DE 1X35, OUTPUTS DE 1X10
+#
+
+numbers = {}
+
+for i in range(10):
+    currNumber = []
+    for j in range(7):
+        currNumber.append(list(map(float, setFileLines[j].rstrip().split(' '))))
+    numbers[i] = np.asmatrix(currNumber).reshape(1, 35)
+
+X = list(numbers.values())
+y = [ np.asmatrix([ 1 if i == j else -1 for i in range(10) ]) for j in range(10) ]
+
+mlp = MultiLayerPerceptron(0.001, tanh, tanhPrime, 35, [25, 15], 10,  a=1e-7, b=1e-4, update=ct.UPDATE_MOMENTUM)
+mlp.train(X, y, adaptative=True)
+
+for i in range(len(X)):
+    print(mlp.classify(X[i]), y[i])
+
+#-----------------------------------------------------#
+# RECONOCE NUMEROS EXPRESADOS EN VECTORES DE 1X10, OUTPUTS DE 1X10
+#
+
+numbers = {}
+
+for i in range(10):
+    currNumber = []
+    for j in range(7):
+        currNumber.append(list(map(float, setFileLines[j].rstrip().split(' '))))
+    numbers[i] = np.asmatrix(currNumber).reshape(1, 35)
+
+X = list(numbers.values())
+X = [ np.asmatrix([ 1 if i == j else -1 for i in range(10) ]) for j in range(10) ]
+y = [ np.asmatrix([ 1 if i == j else -1 for i in range(10) ]) for j in range(10) ]
+
+mlp = MultiLayerPerceptron(0.01, tanh, tanhPrime, 10, [10, 10], 10,  a=1e-7, b=1e-4)
+mlp.train(X, y,  minError = 5e-3, adaptative=True)
 
 for i in range(len(X)):
     print(mlp.classify(X[i]), y[i])
