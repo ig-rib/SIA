c978fbf7f5d43910a62d40210633aad1c78b69da
igrib <igrib@example.com>
2020-05-17 11:22:58 -0300

    added plots for ej1

--- TP3/ej1.py
+++ TP3/ej1.py
@@ -2,6 +2,8 @@
 from stepPerceptron import SimpleStepPerceptron
 from perceptron import Perceptron, SimpleNonLinearPerceptron
 import sigmoidFunctionFactory as sff
+import numpy as np
+import matplotlib.pyplot as plt
 
 andSimplePer = SimpleStepPerceptron(2, 0.5)
 xorSimplePer = SimpleStepPerceptron(2, 0.5)
@@ -23,10 +25,30 @@ def printResults(functionVector, perceptron):
         worked = worked and perceptron.classify(x[0]) == x[1]
     print(f'w = {perceptron.w}')
     print('OK') if worked else print('Not OK')
-    
+
+def plotLine(perceptron, functionVector, title):
+    x = np.linspace(-3, 3, 100)
+    slope = -perceptron.w[1]/perceptron.w[2]
+    intercept = perceptron.w[0]/perceptron.w[2]
+    y = [ xi*slope + intercept for xi in x ]
+    plt.plot(x, y)
+    plusOne = [ x[0] for x in functionVector if x[1] == 1 ]
+    minusOne = [ x[0] for x in functionVector if x[1] == -1 ]
+    plt.scatter([r[0] for r in plusOne], [r[1] for r in plusOne], color='blue')
+    plt.scatter([r[0] for r in minusOne], [r[1] for r in minusOne], color='red')
+    plt.ylim(-3, 3)
+    plt.xlim(-3, 3)
+    plt.title(title)
+    plt.show()
+    print(f'{slope}*x + {intercept}')
+
+
 print('Función AND...\nResultados: ')
 printResults(AndX, andSimplePer)
+plotLine(andSimplePer, AndX, 'AND')
 print('\n\nFunción XOR...\nResultados: ')
 printResults(XorX, xorSimplePer)
+plotLine(xorSimplePer, XorX, 'XOR')
 print('\n\nFunción OR...\nResultados: ')
 printResults(OrX, orSimplePer)
+plotLine(orSimplePer, OrX, 'OR')
