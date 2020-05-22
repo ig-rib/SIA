245824a77e0d8ca1605f57d88dd96f4429288778
igrib <igrib@example.com>
2020-05-22 18:49:49 -0300

    done

--- TP4/ej2.py
+++ TP4/ej2.py
@@ -2,40 +2,112 @@
 
 from hopfield import HopfieldNetwork
 import numpy as np
+import letterUtils as lu
 
+O = -1
+I = 1
 
+patterns = np.asarray([
+    [-1, 1, 1, 1]
+])
 
-a = np.asarray([
-    [ -1, -1, 1, -1, -1  ],
-    [ -1, 1, -1, 1, -1  ],
-    [ -1, 1, 1, 1, -1  ],
-    [ -1, 1, -1, 1, -1  ],
-    [ -1, 1, -1, 1, -1  ]
-    ]).reshape(1, 25)
-b = np.asarray([
-    [ -1, 1, 1, 1, -1  ],
-    [ -1, 1, -1, 1, -1  ],
-    [ -1, 1, 1, 1, -1  ],
-    [ -1, 1, -1, 1, -1  ],
-    [ -1, 1, 1, 1, -1  ]
-    ]).reshape(1, 25)
-b = np.asarray([
-    [ -1, 1, 1, 1, -1  ],
-    [ -1, 1, -1, 1, -1  ],
-    [ -1, 1, 1, 1, -1  ],
-    [ -1, 1, -1, 1, -1  ],
-    [ -1, 1, 1, 1, -1  ]
-    ]).reshape(1, 25)
+hn1 = HopfieldNetwork()
+hn1.store(patterns)
+res, hist = hn1.recognize([1, -1, -1, -1])
+res1, hist1 = hn1.recognize(np.asmatrix([1, -1, -1, -1]))
+res2, hist2 = hn1.recognize([-1, -1, -1, -1])
 
-bje = np.asarray([
-    [ -1, 1, -1, -1, -1  ],
-    [ -1, 1, -1, -1, -1  ],
-    [ -1, 1, 1, 1, -1  ],
-    [ -1, 1, -1, 1, -1  ],
-    [ -1, 1, 1, 1, -1  ]
-    ]).reshape(1, 25)
+print(res)
+print(res1)
+print(res2)
 
+letters = np.asarray(
+[
+np.asarray([
+    [ O, O, I, O, O  ],
+    [ O, I, O, I, O  ],
+    [ I, O, O, O, I  ],
+    [ I, I, I, I, I  ],
+    [ I, O, O, O, I  ]]).reshape(I, 25),
+np.asarray([
+    [ I, I, I, I, O  ],
+    [ I, O, O, O, I  ],
+    [ I, I, I, I, O  ],
+    [ I, O, O, O, I  ],
+    [ I, I, I, I, O  ]]).reshape(I, 25),
+ np.asarray([
+    [ I, I, I, I, I  ],
+    [ I, O, O, O, O  ],
+    [ I, O, O, O, O  ],
+    [ I, O, O, O, O  ],
+    [ I, I, I, I, I  ]]).reshape(I, 25),
+ np.asarray([
+    [ I, I, I, I, O  ],
+    [ I, O, O, O, I  ],
+    [ I, O, O, O, I  ],
+    [ I, O, O, O, I  ],
+    [ I, I, I, I, O  ]]).reshape(I, 25),
+ np.asarray([
+    [ I, I, I, I, I  ],
+    [ I, O, O, O, O  ],
+    [ I, I, I, O, O  ],
+    [ I, O, O, O, O  ],
+    [ I, I, I, I, I  ]]).reshape(I, 25),
+ np.asarray([
+    [ O, I, I, I, O  ],
+    [ O, I, O, O, O  ],
+    [ O, I, I, O, O  ],
+    [ O, I, O, O, O  ],
+    [ O, I, O, O, O  ]]).reshape(I, 25),
+ np.asarray([
+    [ O, I, I, I, O  ],
+    [ O, I, O, O, O  ],
+    [ O, I, O, I, O  ],
+    [ O, I, O, I, O  ],
+    [ O, I, I, I, O  ]]).reshape(I, 25),
+    np.asarray([
+    [ O, I, O, I, O  ],
+    [ O, I, O, I, O  ],
+    [ O, I, I, I, O  ],
+    [ O, I, O, I, O  ],
+    [ O, I, O, I, O  ]]).reshape(I, 25),
+np.asarray([
+    [ O, I, I, I, O  ],
+    [ O, O, I, O, O  ],
+    [ O, O, I, O, O  ],
+    [ O, O, I, O, O  ],
+    [ O, I, I, I, O  ]]).reshape(I, 25)
+]
+)
+
+pickedLetters = letters[:3]
 hn = HopfieldNetwork()
-hn.store(np.asarray([a, b]))
-result = hn.recognize(bje)
-print(result.reshape(5, 5))
+hn.store(pickedLetters)
+
+slightlyNoisyLetters = [lu.addNoiseEverywhere(x, .1) for x in pickedLetters]
+
+# lu.printLetter(pickedLetters[0].reshape(5, 5), 1)
+# lu.printLetter(slightlyNoisyLetters[0].reshape(5, 5), 1)
+
+print('\n\nPicked Letters\n\n')
+lu.printLetters(pickedLetters)
+print('\n#########################\\nSlightly Noisy Letters\n#########################\\n')
+lu.printLetters(slightlyNoisyLetters)
+
+def iterate(letters):
+
+    for l in letters:
+        print('************About to recognize letter: *********** ')
+        lu.printLetter(l.reshape(5, 5), 1)
+        print('**************************************************')
+        res, history = hn.recognize(l)
+        lu.printLetters(history, shape=(5, 5))
+        lu.printLetter(res.reshape(5, 5), 1)
+
+iterate(slightlyNoisyLetters)
+
+reallyNoisyLetters = [lu.addNoiseEverywhere(x, .45) for x in pickedLetters]
+
+print('\n#########################\nReally Noisy Letters\n#########################\n')
+lu.printLetters(reallyNoisyLetters[:1])
+iterate(reallyNoisyLetters[:1])
--- TP4/hopfield.py
+++ TP4/hopfield.py
@@ -6,17 +6,24 @@ class HopfieldNetwork:
 
     def store(self, examples):
         X = np.asmatrix(examples)
-        auxW = [[ 1 / X.shape[1] * sum([X.item(k, i)*X.item(k, j) for k in range(X.shape[0])]) if i != j else 0 for i in range(X.shape[1]) ] for j in range(X.shape[1])]
-        self.w = np.asmatrix(auxW).reshape(X.shape[1], X.shape[1])
-        self.w
+        auxW = [[ (1 / X.shape[1]) * sum([X.item(k, i)*X.item(k, j) for k in range(X.shape[0])]) if i != j else 0 for j in range(X.shape[1]) ] for i in range(X.shape[1])]
+        aaux = np.asmatrix(auxW).reshape(X.shape[1], X.shape[1])
+        self.w = (1 / X.shape[1]) * np.matmul(X.T, X) - np.eye(X.shape[1])
+        self.w = aaux
 
     def recognize(self, x, iterations=1000):
         # S = np.asmatrix(np.tile(x, self.w.shape[0])).reshape(self.w.shape[1], self.w.shape[1])
         # for it in iterations:
         #     S = np.sign(np.dot(self.w, S))
-        S = np.asmatrix(x)
-        oldS = np.zeros(x.shape[1])
-        while (S-oldS).any() != 0:
+        S = np.asmatrix(x).T
+        oldS = np.asmatrix(np.zeros(S.shape[1])).T
+        history = []
+        it = 0
+        # for i in range(iterations):
+        while (oldS - S).any() != 0 and it < iterations:
+        # while it < iterations:
             oldS = S
-            S = np.sign(np.dot(S, self.w))
-        return S
+            history.append(oldS)
+            S = np.sign(np.matmul(self.w, S))
+            it+=1
+        return S, history
