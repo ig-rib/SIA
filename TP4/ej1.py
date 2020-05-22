99ee942af19e0c96d93845f35dbe3cb74e69e0ea
igrib <igrib@example.com>
2020-05-21 23:43:50 -0300

    First working version of Kohonen Net

--- TP4/ej1.py
+++ TP4/ej1.py
@@ -1,1 +1,25 @@
 #!/bin/python3
+
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import math
+import numpy as np
+from kohonenNet import KohonenNetwork
+
+_data = pd.read_csv('TP4/data/europe.csv')
+scaler = MinMaxScaler()
+numericHeaders = [c for c in _data.columns if c != 'Country']
+data = pd.DataFrame(scaler.fit_transform(_data[numericHeaders]), index=_data.index, columns=numericHeaders)
+_data = pd.DataFrame(_data['Country']).join(data)
+
+headers = list(_data.columns)
+D = data.values
+
+numberOfNeurons = int(1*math.sqrt(len(D)))
+weightsVectorLength = len(headers)-1
+
+kn = KohonenNetwork(weightsVectorLength, numberOfNeurons)
+kn.train(D, 100)
+
+for index in range(len(D)):
+    print('Class: ', kn.getClass(D[index]), _data['Country'][index])
