560eea68afa3d205c27fdbc55b41b62ddfa5af0b
igrib <igrib@example.com>
2020-05-23 19:26:56 -0300

    working map of countries

--- TP4/ej1.1.py
+++ TP4/ej1.1.py
@@ -4,7 +4,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import math
 import numpy as np
-from kohonenNet import KohonenNetwork
+from kohonenNet import KohonenNetwork, Coord
+from collections import namedtuple
 
 _data = pd.read_csv('data/europe.csv')
 scaler = StandardScaler()
@@ -19,11 +20,33 @@ D = data.values
 ## PART 1 - Kohonen
 ####################################
 
-numberOfNeurons = int(4)
+side = 5
+
+numberOfNeurons = int(side ** 2)
 weightsVectorLength = len(headers)-1
 
 kn = KohonenNetwork(weightsVectorLength, numberOfNeurons)
-kn.train(D, 1000)
+kn.train(D, 5000, R=3)
+
+map = {}
 
 for index in range(len(D)):
-    print('Class: ', kn.getClass(D[index]), _data['Country'][index])
+    countryClass = kn.getClass(D[index])
+    if map.get(countryClass) == None:
+        map[countryClass] = []
+    map[countryClass].append(_data['Country'][index][0:3])
+    # print('Class: ', kn.getClass(D[index]), _data['Country'][index])
+
+print(map)
+
+for i in range(side):
+    for j in range(side):
+        if map.get(Coord(x=i, y=j)) == None:
+            print('\t\t', end='')
+        else:
+            print(map[Coord(x=i, y=j)], end='')
+            if len(map[Coord(x=i, y=j)]) <= 3:
+                for ii in range(3-len(map[(i, j)])):
+                    print('\t', end='')
+        print(' | ', end='')
+    print()
--- TP4/kohonenNet.py
+++ TP4/kohonenNet.py
@@ -1,16 +1,27 @@
 #!/bin/python3
 
 import numpy as np
+import math
 import random
+from collections import namedtuple
 
-def alpha(s):
-    return 1 / (s+1)
+def alpha(s, totalIterations):
+    return 1 - s / totalIterations
 
 def theta1(i, j, s, R=None):
     return np.abs(i - j) / (s+1)
 
-def theta(i, j, s, R=2):
-    return 1 / (s+1) if np.abs(i-j) < R else 0
+def theta(i, j, s, L, R=2):
+    winnerI = i // L
+    winnerJ = i % L
+    otherI = j // L
+    otherJ = j % L
+    return 1 if math.sqrt((winnerI-otherI) ** 2 + (winnerJ-otherJ) ** 2) <= R else 0
+    # return 1 if np.abs(i-j) <= R else 0
+
+# def theta2(i, j, D, R=2):
+
+Coord = namedtuple('Coord', ['x', 'y'])
 
 class KohonenNetwork:
 
@@ -22,17 +33,19 @@ class KohonenNetwork:
         self.vDim = vectorDimension
         self.M = M
         self.W = np.asmatrix(np.random.random(size=(M, vectorDimension)))
+        self.L = math.sqrt(M) # The side of the square
 
-    def train(self, D, lda, R=4):
-
+    def train(self, D, lda, R=3):
+        indices = list(range(D.shape[0]))
         for it in range(lda):
             curr = random.choice(D)
             distances = [ [np.linalg.norm(curr - x), i] for i, x in enumerate(self.W) ]
             minIndex = min(distances, key=lambda x: x[0])[1]
             for rowNo in range(self.W.shape[0]):
-                self.W[rowNo] = self.W[rowNo] + theta1(minIndex, rowNo, it) * alpha(it) * (curr - self.W[rowNo])
-            # R = 1 if R*0.99 < 1 else R*0.99
-    
+                self.W[rowNo] = self.W[rowNo] + theta(minIndex, rowNo, it, self.L, R) * alpha(it, lda) * (curr - self.W[rowNo])
+            if it % (lda*.1) == 0 and R > 1:
+                R -= 1
     def getClass(self, vector):
         distances = [ [np.linalg.norm(vector - x), i] for i, x in enumerate(self.W) ]
-        return min(distances, key=lambda x: x[0])[1]
+        neuronNo = min(distances, key=lambda x: x[0])[1]
+        return Coord(x=neuronNo // self.L, y=neuronNo % self.L)
