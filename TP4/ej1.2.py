19fb1afd3c592d816d8792df03451348bd7f6c7d
Ignacio Ribas <igrib@example.com>
2020-06-08 15:26:43 -0300

    [Oja] Added 1PC for each country

--- TP4/ej1.2.py
+++ TP4/ej1.2.py
@@ -2,7 +2,7 @@ from ojasRule import OjasRuleNeuron
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
-
+import numpy as np
 
 _data = pd.read_csv('data/europe.csv')
 scaler = StandardScaler()
@@ -21,9 +21,13 @@ D = data.values
 nullCenteredPlainNumbers = justPlainNumbers - justPlainNumbers.mean()
 neu = OjasRuleNeuron(D.shape[1])
 neu.r = 1e-3
-neu.train(D, 10000)
+neu.train(D, 5000)
 print(neu.w)
 
 pca = PCA()
 pca.fit(D)
 print(pca.components_[0])
+firstPC = np.asmatrix(D)*np.asmatrix(pca.components_[0]).T
+fullDataW1PC = pd.concat([fullNormalizedData, pd.DataFrame(firstPC)], axis=1)
+fullDataW1PC.rename({0:'Poverty'}, axis=1, inplace=True)
+print(fullDataW1PC.sort_values('Poverty'))
