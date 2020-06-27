3d3b4f5f4f26e3e81ee98238b437ec301708725e
Ignacio Ribas <igrib@example.com>
2020-06-27 02:42:06 -0300

    b) ... done

--- TP5/ej1.py
+++ TP5/ej1.py
@@ -4,6 +4,7 @@ import numpy as np
 import multiLayerPerceptron as MLP
 import constants as ct
 from sklearn.decomposition import PCA
+from copy import deepcopy
 
 
 setFileLines = open('TP3/ej3Conjunto.tsv').readlines()
@@ -41,15 +42,18 @@ nComp = 12
 #     print()
 #     printNumber(mlp.getLayerOutput(X[i], 2).reshape(7, 5))
 
+# a) Pasar a 2d: Se convierten los enteros a arrays de bits (quedan ejemplos de 7x8, que se pasan a 56x1 para el autoencoder)
 font1m = np.asmatrix(np.array(ct.font1b))
-mlp1 = MLP.MultiLayerPerceptron(0.01, MLP.tanh, MLP.tanhPrime, 56, [nComp], 56, backProp=ct.BP_NO_PRIME)
+# b) Optimizaciones: sacar el término g'
+mlp1 = MLP.MultiLayerPerceptron(0.01, MLP.tanh, MLP.tanhPrime, 56, [24, nComp, 24], 56, backProp=ct.BP_NO_PRIME)
+numberOfLayers = 5
 mlp1.train(font1m, font1m, epochs=2500)
 
 for i in range(font1m.shape[0]):
     # print(mlp.getLayerOutput(font1m[i], 1))
     printNumber(font1m[i].reshape(7, 8))
     print()
-    printNumber(mlp1.getLayerOutput(font1m[i], 1).reshape(4,3))
+    printNumber(mlp1.getLayerOutput(font1m[i], numberOfLayers//2).reshape(4,3))
     print()
     # printNumber(mlp1.getLayerOutput(font1m[i], 2).reshape(7, 8))
     print()
@@ -59,7 +63,32 @@ print("\nNuevas muestras generadas\n")
 import random as rd
 lat1 = np.asmatrix(np.array([1 if rd.random() > 0.5 else -1 for i in range(nComp)]))
 
-lat1Out = mlp1.forwardPropagateFromLayer(lat1, 2)
+lat1Out = mlp1.forwardPropagateFromLayer(lat1, numberOfLayers//2+1)
 printNumber(lat1.reshape(4,3))
 print()
 printNumber(lat1Out.reshape(7,8))
+
+######
+# b)
+######
+
+
+def addNoiseEverywhere(letter, rate):
+    out = letter
+    indices = rd.sample(list(range(letter.shape[1])), int((letter.shape[1] - 1)*rate))
+    for index in indices:
+        out[0, index] = - out.item((0, index))
+    return out
+
+font1mcopy = [addNoiseEverywhere(letter, 0.05) for letter in font1m]
+font1mcopy
+
+for i in range(font1m.shape[0]):
+    # print(mlp.getLayerOutput(font1m[i], 1))
+    printNumber(font1mcopy[i].reshape(7, 8))
+    print()
+    # printNumber(mlp1.getLayerOutput(font1m[i], 1).reshape(4,3))
+    print()
+    printNumber(mlp1.getLayerOutput(font1m[i], numberOfLayers-1).reshape(7, 8))
+    print()
+    print()
