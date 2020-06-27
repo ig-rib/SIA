d40a5915eb59b61614ff02a7024deb1bbfce5ae5
Ignacio Ribas <igrib@example.com>
2020-06-26 23:51:26 -0300

    Added fonts constants for TP5

--- TP5/ej1.py
+++ TP5/ej1.py
@@ -11,7 +11,7 @@ setFileLines = open('TP3/ej3Conjunto.tsv').readlines()
 def printNumber(numMatrix):
     for i in range(numMatrix.shape[0]):
         for j in range(numMatrix.shape[1]):
-            print('%.0g' % (numMatrix.item((i, j))) if numMatrix.item((i, j)) == 1 else ' ', end='')
+            print('%.0g' % 1 if numMatrix.item((i, j)) > 0.5 else ' ', end='') #(numMatrix.item((i, j)))
         print()
 
 numbers = {}
@@ -25,14 +25,19 @@ for i in range(10):
 X = np.asmatrix(np.array(list(numbers.values())))
 Y = np.asmatrix(np.array(list(numbers.values())))
 
-nComp = 10
+nComp = 12
 
 pca = PCA()
 pca.fit(X)
-PCs = np.asmatrix(pca.components_[:nComp])
-asPCs = X * PCs
+# PCs = np.asmatrix(pca.components_[:nComp])
+# asPCs = X * PCs
 mlp = MLP.MultiLayerPerceptron(0.01, MLP.tanh, MLP.tanhPrime, 35, [nComp], 35, backProp=ct.BP_NO_PRIME)
 mlp.train(X, Y, epochs=5000)
 for i in range(X.shape[0]):
-    print(mlp.getLayerOutput(X[i], 1))
+    # print(mlp.getLayerOutput(X[i], 1))
+    printNumber(numbers[i].reshape(7, 5))
+    print()
+    printNumber(mlp.getLayerOutput(X[i], 1).reshape(4,3))
+    print()
+    printNumber(mlp.getLayerOutput(X[i], 2).reshape(7, 5))
 mlp
