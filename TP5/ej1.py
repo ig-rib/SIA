19f8c2ce617a83053fa0e318689da5e79850afd0
Ignacio Ribas <igrib@example.com>
2020-06-27 12:12:23 -0300

    ej1 working

--- TP5/ej1.py
+++ TP5/ej1.py
@@ -15,56 +15,35 @@ def printNumber(numMatrix):
             print('%.0g' % 1 if numMatrix.item((i, j)) > 0.5 else '_', end='') #(numMatrix.item((i, j)))
         print()
 
-# numbers = {}
+nComp = 24
+latRows = 6
+latCols = 4
 
-# for i in range(10):
-#     currNumber = []
-#     for j in range(7):
-#         currNumber.append(list(map(float, setFileLines[i*7+j].rstrip().split(' '))))
-#     numbers[i] = np.asmatrix(currNumber).reshape(1, 35)
-
-# X = np.asmatrix(np.array(list(numbers.values())))
-# Y = np.asmatrix(np.array(list(numbers.values())))
-
-nComp = 12
-
-# pca = PCA()
-# pca.fit(X)
-# PCs = np.asmatrix(pca.components_[:nComp])
-# asPCs = X * PCs
-# mlp = MLP.MultiLayerPerceptron(0.01, MLP.tanh, MLP.tanhPrime, 35, [nComp], 35, backProp=ct.BP_NO_PRIME)
-# mlp.train(X, Y, epochs=5000)
-# for i in range(X.shape[0]):
-#     # print(mlp.getLayerOutput(X[i], 1))
-#     printNumber(numbers[i].reshape(7, 5))
-#     print()
-#     printNumber(mlp.getLayerOutput(X[i], 1).reshape(4,3))
-#     print()
-#     printNumber(mlp.getLayerOutput(X[i], 2).reshape(7, 5))
-
-# a) Pasar a 2d: Se convierten los enteros a arrays de bits (quedan ejemplos de 7x8, que se pasan a 56x1 para el autoencoder)
+# 1) Pasar a 2d: Se convierten los enteros a arrays de bits (quedan ejemplos de 7x8, que se pasan a 56x1 para el autoencoder)
 font1m = np.asmatrix(np.array(ct.font1b))
-# b) Optimizaciones: sacar el término g'
-mlp1 = MLP.MultiLayerPerceptron(0.01, MLP.tanh, MLP.tanhPrime, 56, [24, nComp, 24], 56, backProp=ct.BP_NO_PRIME)
-numberOfLayers = 5
+# 2) Optimizaciones: sacar el término g'
+mlp1 = MLP.MultiLayerPerceptron(0.01, MLP.tanh, MLP.tanhPrime, 56, [nComp], 56, backProp=ct.BP_NO_PRIME)
+numberOfLayers = len(mlp1.wByLayer.keys())
 mlp1.train(font1m, font1m, epochs=2500)
 
+# 3)
 for i in range(font1m.shape[0]):
     # print(mlp.getLayerOutput(font1m[i], 1))
     printNumber(font1m[i].reshape(7, 8))
     print()
-    printNumber(mlp1.getLayerOutput(font1m[i], numberOfLayers//2).reshape(4,3))
+    printNumber(mlp1.getLayerOutput(font1m[i], numberOfLayers//2).reshape(latRows,latCols))
     print()
     # printNumber(mlp1.getLayerOutput(font1m[i], 2).reshape(7, 8))
     print()
     print()
 
+# 4)
 print("\nNuevas muestras generadas\n")
 import random as rd
 lat1 = np.asmatrix(np.array([1 if rd.random() > 0.5 else -1 for i in range(nComp)]))
 
 lat1Out = mlp1.forwardPropagateFromLayer(lat1, numberOfLayers//2+1)
-printNumber(lat1.reshape(4,3))
+printNumber(lat1.reshape(latRows,latCols))
 print()
 printNumber(lat1Out.reshape(7,8))
 
@@ -72,6 +51,9 @@ printNumber(lat1Out.reshape(7,8))
 # b)
 ######
 
+# 1) Se define arriba. No hay una justifición formal para la elección, es heurística
+#   Agregar capas intermedias no parece hacer una gran diferencia en los resultados,
+#   sólo (naturalmente) aumenta el tiempo de ejecución
 
 def addNoiseEverywhere(letter, rate):
     out = letter
@@ -80,15 +62,32 @@ def addNoiseEverywhere(letter, rate):
         out[0, index] = - out.item((0, index))
     return out
 
-font1mcopy = [addNoiseEverywhere(letter, 0.05) for letter in font1m]
-font1mcopy
+# 2)
 
-for i in range(font1m.shape[0]):
-    # print(mlp.getLayerOutput(font1m[i], 1))
-    printNumber(font1mcopy[i].reshape(7, 8))
-    print()
-    # printNumber(mlp1.getLayerOutput(font1m[i], 1).reshape(4,3))
-    print()
-    printNumber(mlp1.getLayerOutput(font1m[i], numberOfLayers-1).reshape(7, 8))
-    print()
-    print()
+# Ruido 5%
+font1m5 = [addNoiseEverywhere(letter, 0.05) for letter in deepcopy(font1m)]
+# Ruido 15%
+font1m15 = [addNoiseEverywhere(letter, 0.15) for letter in deepcopy(font1m)]
+# Ruido 25%
+font1m25 = [addNoiseEverywhere(letter, 0.25) for letter in deepcopy(font1m)]
+
+noisySamples = {0.05: font1m5, 0.15: font1m15, 0.25: font1m25}
+
+## Utility function for plotting input-output given sample noise
+def plotForPercentage(r):
+    for i in range(font1m.shape[0]):
+        # print(mlp.getLayerOutput(font1m[i], 1))
+        print('Original input')
+        printNumber(font1m[i].reshape(7, 8))
+        print()
+        print(f'Noisy input {r}:')
+        printNumber(noisySamples[r][i].reshape(7, 8))
+        print()
+        # printNumber(mlp1.getLayerOutput(font1m[i], 1).reshape(latRows,latCols))
+        print('Autoencoder output')
+        printNumber(mlp1.getLayerOutput(font1m[i], numberOfLayers).reshape(7, 8))
+        print()
+        print()
+
+for r in noisySamples.keys():
+    plotForPercentage(r)
